6d4a078071c2a897851a2003bcec5a7ca96e6b3f
ChristopherTechPrep <cmazon32@gmail.com>
2023-03-02 18:12:55 +0000

    RandomResults Random Results working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb" })
+  end
 
+  def calculate_square
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num ** 2.0
+    render({ :template => "calculation_templates/square_results.html.erb" })
+  end
+
+  def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    render({ :template => "calculation_templates/ran_results.html.erb" })
+  end
 end
